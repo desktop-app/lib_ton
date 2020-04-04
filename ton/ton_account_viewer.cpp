desktop-app/lib_ton

commit 8672aa9d552aba51daa2d9e3c29a6016eb585d8c

Support silent and explicit decrypt requests.

ton/ton_account_viewer.cpp
@@ -69,20 +69,22 @@ void AccountViewer::preloadSlice(const TransactionId &lastId) {
 			_loadedResults.fire(std::move(result.error()));
 			return;
 		}
-		auto last = std::move(*result);
-		const auto encrypted = CollectEncryptedTexts(last);
-		const auto done = [=](Result<QVector<DecryptedText>> result) {
+		const auto previousId = result->previousId;
+		const auto done = [=](Result<std::vector<Transaction>> &&result) {
 			if (!result) {
 				_loadedResults.fire(std::move(result.error()));
 				return;
 			}
+			auto slice = TransactionsSlice();
+			slice.previousId = previousId;
+			slice.list = std::move(*result);
 			_preloadIds.remove(lastId);
 			_loadedResults.fire(LoadedSlice{
 				lastId,
-				AddDecryptedTexts(last, encrypted, *result)
+				TransactionsSlice{ std::move(*result), previousId }
 			});
 		};
-		_wallet->decryptTexts(_publicKey, encrypted, done);
+		_wallet->trySilentDecrypt(_publicKey, std::move(result->list), done);
 	};
 	_wallet->requestTransactions(
 		_publicKey,

ton/details/ton_account_viewers.cpp
@@ -197,21 +197,17 @@ void AccountViewers::saveNewStateEncrypted(
 			std::move(pending)
 		}, source);
 	};
-	const auto encrypted = CollectEncryptedTexts(full.lastTransactions);
-	if (!encrypted.isEmpty()) {
-		const auto done = [=](Result<QVector<DecryptedText>> result) {
-			const auto viewers = findRefreshingViewers(address);
-			if (!viewers || reportError(*viewers, result)) {
-				return;
-			}
-			finish(
-				*viewers,
-				AddDecryptedTexts(full.lastTransactions, encrypted, *result));
-		};
-		_owner->decryptTexts(viewers.publicKey, encrypted, done);
-	} else {
-		finish(viewers, std::move(last));
-	}
+	const auto previousId = last.previousId;
+	const auto done = [=](Result<std::vector<Transaction>> &&result) {
+		const auto viewers = findRefreshingViewers(address);
+		if (!viewers || reportError(*viewers, result)) {
+			return;
+		}
+		finish(
+			*viewers,
+			TransactionsSlice{ std::move(*result), previousId });
+	};
+	_owner->trySilentDecrypt(viewers.publicKey, std::move(last.list), done);
 }
 
 void AccountViewers::checkNextRefresh() {

ton/details/ton_parse_state.cpp
@@ -218,15 +218,16 @@ QVector<DecryptedText> MsgDataArrayToDecrypted(
 	});
 }
 
-QVector<EncryptedText> CollectEncryptedTexts(const TransactionsSlice &data) {
+QVector<EncryptedText> CollectEncryptedTexts(
+		const std::vector<Transaction> &data) {
 	auto result = QVector<EncryptedText>();
-	result.reserve(data.list.size());
+	result.reserve(data.size());
 	const auto add = [&](const Message &data) {
 		if (!data.message.encrypted.isEmpty()) {
 			result.push_back({ data.message.encrypted, data.source });
 		}
 	};
-	for (const auto &transaction : data.list) {
+	for (const auto &transaction : data) {
 		add(transaction.incoming);
 		for (const auto &out : transaction.outgoing) {
 			add(out);
@@ -235,8 +236,8 @@ QVector<EncryptedText> CollectEncryptedTexts(const TransactionsSlice &data) {
 	return result;
 }
 
-TransactionsSlice AddDecryptedTexts(
-		TransactionsSlice parsed,
+std::vector<Transaction> AddDecryptedTexts(
+		std::vector<Transaction> parsed,
 		const QVector<EncryptedText> &encrypted,
 		const QVector<DecryptedText> &decrypted) {
 	Expects(encrypted.size() == decrypted.size());
@@ -255,7 +256,7 @@ TransactionsSlice AddDecryptedTexts(
 			message.message.decrypted = true;
 		}
 	};
-	for (auto &transaction : parsed.list) {
+	for (auto &transaction : parsed) {
 		decrypt(transaction.incoming);
 		for (auto &out : transaction.outgoing) {
 			decrypt(out);

ton/details/ton_parse_state.h
@@ -29,9 +29,9 @@ namespace Ton::details {
 	const TLmsg_DataDecryptedArray &data);
 
 [[nodiscard]] QVector<EncryptedText> CollectEncryptedTexts(
-	const TransactionsSlice &data);
-[[nodiscard]] TransactionsSlice AddDecryptedTexts(
-	TransactionsSlice parsed,
+	const std::vector<Transaction> &data);
+[[nodiscard]] std::vector<Transaction> AddDecryptedTexts(
+	std::vector<Transaction> parsed,
 	const QVector<EncryptedText> &encrypted,
 	const QVector<DecryptedText> &decrypted);
 

ton/ton_wallet.cpp
@@ -34,6 +34,10 @@ using namespace details;
 
 constexpr auto kSmcRevision = 1;
 
+[[nodiscard]] TLError GenerateFakeIncorrectPasswordError() {
+	return tl_error(tl_int32(0), tl_string("KEY_DECRYPT"));
+}
+
 } // namespace
 
 Wallet::Wallet(const QString &path)
@@ -511,32 +515,73 @@ void Wallet::requestTransactions(
 	}).send();
 }
 
-void Wallet::decryptTexts(
+void Wallet::trySilentDecrypt(
 		const QByteArray &publicKey,
-		const QVector<EncryptedText> &encrypted,
-		Callback<QVector<DecryptedText>> done) {
-	if (encrypted.isEmpty()) {
-		InvokeCallback(done, QVector<DecryptedText>());
+		std::vector<Transaction> &&list,
+		Callback<std::vector<Transaction>> done) {
+	const auto encrypted = CollectEncryptedTexts(list);
+	if (encrypted.empty() || !_viewersPasswords.contains(publicKey)) {
+		InvokeCallback(done, std::move(list));
 		return;
 	}
+	const auto shared = std::make_shared<std::vector<Transaction>>(
+		std::move(list));
 	const auto password = _viewersPasswords[publicKey];
 	const auto generation = password.generation;
 	_external->lib().request(TLmsg_Decrypt(
 		prepareInputKey(publicKey, password.bytes),
 		MsgDataArrayFromEncrypted(encrypted)
 	)).done([=](const TLmsg_DataDecryptedArray &result) {
-		_updates.fire({ DecryptPasswordGood{ generation } });
-		InvokeCallback(done, MsgDataArrayToDecrypted(result));
+		InvokeCallback(
+			done,
+			AddDecryptedTexts(
+				std::move(*shared),
+				encrypted,
+				MsgDataArrayToDecrypted(result)));
 	}).fail([=](const TLError &error) {
+		InvokeCallback(done, std::move(*shared));
+	}).send();
+}
+
+void Wallet::decrypt(
+		const QByteArray &publicKey,
+		std::vector<Transaction> &&list,
+		Callback<std::vector<Transaction>> done) {
+	const auto encrypted = CollectEncryptedTexts(list);
+	if (encrypted.empty()) {
+		InvokeCallback(done, std::move(list));
+		return;
+	}
+	const auto shared = std::make_shared<std::vector<Transaction>>(
+		std::move(list));
+	const auto password = _viewersPasswords[publicKey];
+	const auto generation = password.generation;
+	const auto fail = [=](const TLError &error) {
 		handleInputKeyError(publicKey, generation, error, [=](
 				Result<> result) {
 			if (result) {
-				decryptTexts(publicKey, encrypted, done);
+				decrypt(publicKey, std::move(*shared), done);
 			} else {
 				InvokeCallback(done, result.error());
 			}
 		});
-	}).send();
+	};
+	if (password.bytes.isEmpty()) {
+		fail(GenerateFakeIncorrectPasswordError());
+		return;
+	}
+	_external->lib().request(TLmsg_Decrypt(
+		prepareInputKey(publicKey, password.bytes),
+		MsgDataArrayFromEncrypted(encrypted)
+	)).done([=](const TLmsg_DataDecryptedArray &result) {
+		_updates.fire({ DecryptPasswordGood{ generation } });
+		InvokeCallback(
+			done,
+			AddDecryptedTexts(
+				std::move(*shared),
+				encrypted,
+				MsgDataArrayToDecrypted(result)));
+	}).fail(fail).send();
 }
 
 void Wallet::handleInputKeyError(

ton/ton_wallet.h
@@ -94,6 +94,15 @@ public:
 
 	void loadWebResource(const QString &url, Callback<QByteArray> done);
 
+	void decrypt(
+		const QByteArray &publicKey,
+		std::vector<Transaction> &&list,
+		Callback<std::vector<Transaction>> done);
+	void trySilentDecrypt(
+		const QByteArray &publicKey,
+		std::vector<Transaction> &&list,
+		Callback<std::vector<Transaction>> done);
+
 	// Internal API.
 	void requestState(const QString &address, Callback<AccountState> done);
 	void requestTransactions(
@@ -101,10 +110,6 @@ public:
 		const QString &address,
 		const TransactionId &lastId,
 		Callback<TransactionsSlice> done);
-	void decryptTexts(
-		const QByteArray &publicKey,
-		const QVector<EncryptedText> &encrypted,
-		Callback<QVector<DecryptedText>> done);
 
 private:
 	struct ViewersPassword {

commit c2d6305301bbfec55932e3597f6e2f67097b8766

Fix preloading old transactions.
Regression was introduced in 8672aa9d55.

ton/ton_account_viewer.cpp
@@ -75,9 +75,6 @@ void AccountViewer::preloadSlice(const TransactionId &lastId) {
 				_loadedResults.fire(std::move(result.error()));
 				return;
 			}
-			auto slice = TransactionsSlice();
-			slice.previousId = previousId;
-			slice.list = std::move(*result);
 			_preloadIds.remove(lastId);
 			_loadedResults.fire(LoadedSlice{
 				lastId,